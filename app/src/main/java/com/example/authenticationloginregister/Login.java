b9f4053d4e9819f70d5015aceac46a4621dbf829
Kuba Downarowicz <kubadownar444@gmail.com>
2021-05-08 16:49:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/authenticationloginregister/Login.java
@@ -0,0 +1,141 @@
+package com.example.authenticationloginregister;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
+
+public class Login extends AppCompatActivity {
+
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn, forgotTextLink;
+    FirebaseAuth fAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.eMail);
+        mPassword = findViewById(R.id.Password1);
+        mLoginBtn = findViewById(R.id.loginBTN);
+        fAuth = FirebaseAuth.getInstance();
+        mCreateBtn = findViewById(R.id.forgotPassword);
+        forgotTextLink = findViewById(R.id.forgotPassword);
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required");
+                    return;
+                }
+
+                if(password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 characters");
+                    return;
+                }
+
+
+                //autoryzacja uzytkownika
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Logged in Successfully!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        } else {
+                            Toast.makeText(Login.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    final EditText resetMail = new EditText(v.getContext());
+                    AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                    passwordResetDialog.setTitle("Reset Password ?");
+                    passwordResetDialog.setMessage("Enter your email to received reset link. ");
+                    passwordResetDialog.setView(resetMail);
+
+                    passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //wyslanie linku
+
+                            String mail = resetMail.getText().toString();
+                            fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Login.this, "Reset Link Sent to Your Email.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Login.this, "Error! Reset Link is not Sent to Your Email." + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
+
+                        }
+                    });
+
+                    passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // zamykanie okienka
+
+
+                        }
+                    });
+
+                    passwordResetDialog.create().show();
+
+            }
+        });
+
+    }
+}
